6fb692b14b69bff40c91387b423432a02fee3bf2
googleyufei <googleyufei@qq.com>
2015-09-17 10:06:24 +0800

    update templates

--- templates/fis-conf.js
+++ templates/fis-conf.js
@@ -1,60 +1,89 @@
-var enableConf = {
-  useSprite: true,
-  useOptimize: false,
-  userName: '',
-  projectName: ''
-};
+fis.set("atm", {
+    useSprite: true, // 是否在开发阶段使用雪碧图合并
+    useOptimize: false, // 是否压缩css
+    useHash: false, // 是否给文件名加上hash值
+    userName: '',  // RTX用户名
+    projectName: '', // 项目名称
+    cdnPath: '' // 上传到CDN的路径
+});
 
 fis.set('project.files', ['**', '.**', '.**/**'])
-  .set('project.ignore', ['node_modules/**', '.gitignore', '**/_*.scss', '.docs/**', 'publish/**', '.dist/**', '.git/**', '.svn/**', 'fis-conf.js']);
+    .set('project.ignore', ['node_modules/**', '.idea/**', '.gitignore', '**/_*.scss', '.docs/**', 'publish/**', '.dist/**', '.git/**', '.svn/**', 'gruntfile.js', 'gulpfile.js', 'fis-conf.js']);
 
 fis.hook('relative');
 
+if (!!fis.get("atm").cdnPath) {
+    fis.get("atm").useDomain = !!fis.get("atm").cdnPath;
+    fis.get("atm").domain = "http://3gimg.qq.com/mig-web/" + fis.get("atm").cdnPath;
+}
+
+var atmConf = fis.get("atm");
+
 /*************************目录规范*****************************/
 fis.match('*', {
-  useHash: false,
-  relative: true,
-  _isResourceMap: false
+    relative: true,
+    useHash: false,
+    useDomain: false,
+    domain: atmConf.domain,
+    _isResourceMap: false
 }).match(/.*\.(html|htm|php)$/, { //页面模板不用编译缓存
-  useCache: false
+    useCache: false,
 }).match("/css/**.{css,less,scss}", {
-  useSprite: enableConf.useSprite,
-  optimizer: enableConf.useOptimize && fis.plugin('clean-css')
+    useSprite: atmConf.useSprite,
+    useDomain: atmConf.useDomain,
+    useHash: atmConf.useHash,
+    optimizer: atmConf.useOptimize && fis.plugin('clean-css')
 }).match('/css/**.less', {
-  rExt: '.css',
-  parser: fis.plugin('less')
-//}).match('/css/**.scss', {
-//  rExt: '.css',
-//  parser: fis.plugin('scss')
+    rExt: '.css',
+    parser: fis.plugin('less')
 }).match('*.mixin.less', {//less的mixin文件无需发布
-  release: false
+    release: false
 }).match("/design/**.psd", {
-  release: false
-}).match("font/**", {}).match("img/**", {}).match('img/**.png', {
-  optimizer: fis.plugin('png-compressor')
-}).match('js/**', {}).match('mail/**', {}).match('slice/**', {});
+    release: false
+}).match("font/**", {
+    useHash: atmConf.useHash,
+    useDomain: atmConf.useDomain
+}).match("img/**", {
+    useDomain: atmConf.useDomain,
+    useHash: atmConf.useHash
+}).match('img/**.png', {
+    optimizer: fis.plugin('png-compressor')
+}).match('js/**', {
+    useDomain: atmConf.useDomain,
+    useHash: atmConf.useHash
+}).match('mail/**', {}).match('slice/**', {});
 
 fis.match('**', {
-  deploy: fis.plugin('local-deliver', {
-    to: './publish'
-  })
+    deploy: fis.plugin('local-deliver', {
+        to: './publish'
+    })
 }).match("::packager", {
-  spriter: fis.plugin('csssprites', {
-    htmlUseSprite: true,
-    layout: 'matrix',
-    margin: '15',
-    scale: 0.5,
-    styleReg: /(<style(?:(?=\s)[\s\S]*?["'\s\w\/\-]>|>))([\s\S]*?)(<\/style\s*>|$)/ig
-  })
+    spriter: fis.plugin('csssprites', {
+        htmlUseSprite: true,
+        layout: 'matrix',
+        margin: '16',
+        scale: 0.5,
+        styleReg: /(<style(?:(?=\s)[\s\S]*?["'\s\w\/\-]>|>))([\s\S]*?)(<\/style\s*>|$)/ig
+    })
 });
 
 fis.media('test').match("/css/**.{css,less}", {
-  useSprite: true,
-  optimizer: enableConf.useOptimize && fis.plugin('clean-css')
+    useSprite: true,
+    optimizer: atmConf.useOptimize && fis.plugin('clean-css')
 }).match('**', {
-  deploy: fis.plugin('http-push', {
-    receiver: 'http://ued.wsd.com/receiver/receiver2.php',
-    to: '/data/wapstatic/' + enableConf.userName + '/' + enableConf.projectName
-  })
+    deploy: fis.plugin('http-push', {
+        receiver: 'http://ued.wsd.com/receiver/receiver2.php',
+        to: '/data/wapstatic/' + atmConf.userName + '/' + atmConf.projectName
+    })
+});
+
+fis.media('cdn').match("/css/**.{css,less}", {
+    useSprite: true,
+    optimizer: atmConf.useOptimize && fis.plugin('clean-css')
+}).match('**', {
+    deploy: fis.plugin('cdn', {
+        remoteDir: atmConf.cdnPath,
+        uploadUrl: 'http://super.kf0309.3g.qq.com/qm/upload'
+    })
 });
 
